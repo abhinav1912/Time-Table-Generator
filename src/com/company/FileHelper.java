b736969a7202978c8d1c04886841900697e41445
abhinav1912 <abhinavmathur1912@gmail.com>
2021-02-07 23:53:43 +0530

    added file i/o

--- src/com/company/FileHelper.java
+++ src/com/company/FileHelper.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileHelper {
     public static boolean createFile(String filename){
@@ -16,7 +21,18 @@ public class FileHelper {
         }
     }
 
-    public static boolean checkFile(String filename){
+    public static Map<String, String[]> initialiseStreamList(){
+        Map<String, String[]> streamlist = new HashMap<String, String[]>();
+        String[] science = new String[] {"English", "Math", "Physics", "Chemistry", "CS"};
+        String[] commerce = new String[] {"English", "Math", "Accounts", "Economics", "Business"};
+        String[] arts = new String[] {"English", "Politics", "History", "Economics", "Psychology"};
+        streamlist.put("science", science);
+        streamlist.put("commerce", commerce);
+        streamlist.put("arts", arts);
+        return streamlist;
+    }
+
+    public static boolean checkFileExistence(String filename){
         File temp = new File(filename);
         if (temp.exists()) return true;
         else return false;
@@ -27,8 +43,28 @@ public class FileHelper {
         return "";
     }
 
-    public static boolean writeFile(String filename, String data){
-        // process
-        return true;
+    public static boolean writeFile(String filename, Map<String, String[]> data){
+        File file = new File(filename);
+        BufferedWriter bf = null;
+        boolean success = true;
+
+        try {
+            bf = new BufferedWriter(new FileWriter(file));
+            for(Map.Entry<String, String[]> entry : data.entrySet()){
+                bf.write(entry.getKey()+":"+Arrays.toString(entry.getValue()));
+                bf.newLine();
+            }
+
+            bf.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+            success = false;
+        }finally {
+            try{
+                bf.close();
+            }catch (Exception e){}
+        }
+        return success;
     }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,20 +9,32 @@ public class Main {
         System.out.println("");
         System.out.println("Checking if stream data exists.");
         String streamFile = "streams.txt";
-        boolean fileExists = FileHelper.checkFile(streamFile);
+        boolean fileExists = FileHelper.checkFileExistence(streamFile);
 
         if (!fileExists){
-            System.out.println("stream.txt not found, attempting initialisation. \n");
+            System.out.println("streams.txt not found, attempting initialisation. \n");
             boolean fileCreated = FileHelper.createFile(streamFile);
             if (!fileCreated){
                 System.out.println("Terminating execution.");
                 return;
+            }
+            else {
+                System.out.println(streamFile+" successfully created.");
             }
         }
         else {
             System.out.println("File found, skipping initialisation.");
         }
 
+        System.out.println("Attempting Initialisation.");
+        Map<String, String[]> streams = FileHelper.initialiseStreamList();
+        boolean success = FileHelper.writeFile(streamFile, streams);
+        if (success) {
+            System.out.println("Success.");
+        }
+        else {
+            System.out.println("Fail");
+        }
 
     }
 }
